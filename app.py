26e36b244ddc171c911a0c01e2975557c03d77e8
Tejpal Sahota <tssahota@uwaterloo.ca>
2018-04-26 20:41:47 -0400

    Attempting to get Savoir running

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from Savoir import Savoir
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    rpcuser = 'multichainrpc'
+    rpcpasswd = 'CTf7sJBaxzQbNB3gWBUqE3VRD4xKYNirjeTLSVk6LWej'
+    rpchost = 'localhost'
+    rpcport = '6460'
+    chainname = 'chain1'
+    api = Savoir(rpcuser, rpcpasswd, rpchost, rpcport, chainname)
+    return "Hello World!" + api.getinfo()
+    
